886a3204facd8f095c7754a5c4790ee7792a11ef
babusabin <pathaksabin88@gmail.com>
2018-06-19 19:23:03 +0545

    Initial commit

---
+++ Automatic_Image_Colorizer/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Automatic_Image_Colorizer project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Automatic_Image_Colorizer.settings")
+
+application = get_wsgi_application()
